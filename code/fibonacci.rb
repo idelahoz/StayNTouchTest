6df6ad23533236aad109e852b930bf5824b64ba4
Israel De La Hoz <israeldelahoz@gmail.com>
2018-11-14 11:51:26 -0500

    Fibonacci case: I used the inject method starting with an array [0,1] so I could access previous results then I add the new result in that array and keep iterating. I avoided recursion to low down memory usage.

--- code/fibonacci.rb
+++ code/fibonacci.rb
@@ -2,5 +2,7 @@ class Fibonacci
   # Implement a method that will calculate the Nth number of the Fibonacci
   # sequence (http://en.wikipedia.org/wiki/Fibonacci_number)
   def self.calculate(n)
+    return 0 if n == 0
+    (1...n).inject( [0, 1] ) { | sequence | sequence << sequence.last(2).sum }.last
   end
 end
