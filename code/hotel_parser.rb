b2c891b9f51a983ee5fa7421d13854787a19e6a3
Israel De La Hoz <israeldelahoz@gmail.com>
2018-11-14 13:39:48 -0500

    Hotel parser case: I created a class called HotelObject, It's a wrapper for the hotel data and responds to the methods with the name as the hash keys. The wrapper created a new instance of itself for hashes that are inside arrays or other hashes but it only does it on demand so no extra objects are loaded into memory.

--- code/hotel_parser.rb
+++ code/hotel_parser.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class HotelParser
   # Implement a method parse the contents of a YAML file and return
   # an object whose values are accessible using the [] operator or method calls
@@ -5,5 +7,33 @@ class HotelParser
   # Note: Use of the YAML library is fine (encouraged, even) but please don't
   #       use any third-party gems that enable the required functionality.
   def self.parse(filename)
+    HotelObject.new(YAML.load_file(filename))
+  end
+
+  class HotelObject
+
+    def initialize(data)
+      @data = data
+    end
+
+    def [](key)
+      data[key]
+    end
+
+    private
+
+    attr_reader :data
+
+
+    def method_missing(method, *args, &block)
+      return process(data[method.to_s]) if data[method.to_s]
+
+      raise ArgumentError.new("Method `#{method}` doesn't exist.")
+    end
+
+    def process(data)
+      return data.map {|datum| process(datum) } if data.is_a? Array
+      data.is_a?(Hash) ? HotelObject.new(data) : data
+    end
   end
 end
